64aecf5ce094a201089511d70ddb28b768e243f4
TimDovg <timdovg@gmail.com>
2019-07-11 13:36:40 +0300

    Check user button

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+/*
 import 'babel-polyfill';
 import _ from 'lodash';
 
@@ -22,3 +23,90 @@ const o = {
 };
 
 console.log(o?.foo?.bar?.baz ?? 'default');
+*/
+
+
+/*
+const API_URL = 'https://studentschat.herokuapp.com/';
+
+async function startApp() {
+  try {
+    const endpoint = 'users';
+    const users = await callApi(endpoint, 'GET');
+    return getUsersNames(users);
+  } catch (error) {
+    console.warn(error);
+  } finally {
+
+  }
+}
+
+function callApi(endpoind, method) {
+  const url = API_URL + endpoind;
+  const options = {
+    method
+  };
+
+  return fetch(url, options)
+      .then(response => response.ok ? response.json() : Promise.reject(Error('Failed to load')))
+      .then(file => JSON.parse(atob(file.content)))
+      .catch(error => { throw error });
+}
+
+function getUsersNames(users) {
+  const names = users.map(it => it.username).join('\n');
+  return names;
+}
+startApp();*/
+
+
+  let usernames = [];
+
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://studentschat.herokuapp.com/users', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      // Обработчик успещного ответа
+      var response = request.responseText;
+      userList = Response;
+
+      let users = JSON.parse(response).map(
+          function (obj) {
+            return obj.username;
+          }
+      )
+
+      for (let i = 0; i < users.length; i++) {
+        usernames.push(users[i]);
+      }
+
+    } else {
+      // Обработчик ответа в случае ошибки
+    }
+  };
+  request.onerror = function() {
+    // Обработчик ответа в случае неудачного соеденения
+  };
+  request.send();
+
+
+function checkUser() {
+  var user = document.getElementsByTagName("input")[0];
+  user = user.value;
+
+  let allow = false;
+  usernames.forEach(
+      function (usr) {
+        if (usr == user) allow = true;
+      }
+  );
+
+  if (allow) {
+    alert('Привет, ' + user + '!');
+    window.location.replace("https://timdovg.github.io/Chat/src/index.html");
+  }
+  else alert('Пользователь не найден! \n' +
+      'Пожалуйста, зарегистрируйтесь!');
+
+}
