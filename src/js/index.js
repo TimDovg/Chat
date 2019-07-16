25f178e061a06dd00998c6eaf48d6074778757a3
TimDovg <timdovg@gmail.com>
2019-07-16 15:23:36 +0300

    merged js, fixed dislay users update

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,106 @@
+document.getElementById('wrapp').style.display = "none";
+
+// аутентификация
+let usernames = []; // only names
+let userNow = ''; // пользователь, который вошел
+let userNowId = ''; // его Id
+let users = []; // объект с пользователями (users = [{} {} {}]  прям все, что на бэкэ)
+
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://studentschat.herokuapp.com/users', false);
+
+request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+        // Обработчик успещного ответа
+        var response = request.responseText;
+        userList = Response;
+
+        let users = JSON.parse(response).map(
+            function (obj) {
+                return obj.username;
+            }
+        )
+
+        for (let i = 0; i < users.length; i++) {
+            usernames.push(users[i]);
+        }
+
+    } else {
+        // Обработчик ответа в случае ошибки
+    }
+};
+request.onerror = function() {
+    // Обработчик ответа в случае неудачного соеденения
+};
+request.send();
+
+
+
+function checkUser() {
+    var user = document.getElementsByTagName("input")[0];
+    user = user.value;
+
+    let allow = false;
+    usernames.forEach(
+        function (usr) {
+            if (usr == user) allow = true;
+        }
+    );
+
+    if (allow) {
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].username == userNow) {
+                userNowId = users[i].user_id;
+                break;
+            }
+        }
+        userNow = user;
+        document.getElementById('userNow').innerHTML = 'Привет, ' + userNow + '!';
+        document.getElementById('registr').style.display = "none";
+        document.getElementById('wrapp').style.display = "block";
+    }
+    else {
+        alert('Пользователь не найден! \n' +
+            'Пожалуйста, зарегистрируйтесь!');
+    }
+
+}
+
+
+function registerUser() {
+    var user = document.getElementsByTagName("input")[0];
+    user = user.value;
+    let allow = true;
+
+    usernames.forEach(
+        function (usr) {
+            if (usr == user) allow = false;
+        }
+    );
+
+    if (!allow) return alert('Пользователь с таким именем уже сущуствует!');
+
+    var request1 = new XMLHttpRequest();
+    request1.open('POST', 'https://studentschat.herokuapp.com/users/register', false);
+
+    request1.onload = function() {
+        // Обработчик ответа в случае удачного соеденения
+    };
+
+    request1.onerror = function() {
+        alert('error')// Обработчик ответа в случае неудачного соеденения
+    };
+    request1.setRequestHeader('Content-Type', 'application/json');
+
+    request1.send(JSON.stringify({username: user}));
+
+    alert('Вы успешно зарегистрированы!');
+    window.location.reload();
+}
+
+
+
 // вывод времени и подсчет вермени в онлайне
 let min = -1;
 window.onload = function(){
@@ -31,7 +134,7 @@ window.onload = function(){
 
 // вывод пользователей
 displayUsers();
-setTimeout(displayUsers, 10000);
+setInterval(displayUsers, 10000);
 
 
 
@@ -192,8 +295,39 @@ function onSearch(input) {
 
 
 
-// users = [{} {} {}]  прям все, что на бэкэ
 function displayUsers() {
+    users = [];
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://studentschat.herokuapp.com/users', false);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Обработчик успещного ответа
+            var response = request.responseText;
+            userList = Response;
+
+            let usersGet = JSON.parse(response).map(
+                function (obj) {
+                    return obj;
+                }
+            )
+
+            for (let i = 0; i < usersGet.length; i++) {
+                users.push({});
+                for (key in usersGet[i]) {
+                    users[i][key] = usersGet[i][key];
+                }
+            }
+
+        } else {
+            // Обработчик ответа в случае ошибки
+        }
+    };
+    request.onerror = function() {
+        // Обработчик ответа в случае неудачного соеденения
+    };
+    request.send();
+
     // стрираем предыдущие
     let friends = document.getElementsByClassName('friend');
     while (friends.length) {
